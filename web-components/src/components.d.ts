0f2952ed7855667a56f94f7e546f20a1f661d5be
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-10-06 16:14:31 +0800

    feat: readr header (#1)
    * feat: add base.css
    
    * feat: add readr-header

--- web-components/src/components.d.ts
+++ web-components/src/components.d.ts
@@ -8,6 +8,8 @@ import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
     interface LatestCoverages {
     }
+    interface ReadrHeader {
+    }
 }
 declare global {
     interface HTMLLatestCoveragesElement extends Components.LatestCoverages, HTMLStencilElement {
@@ -16,15 +18,25 @@ declare global {
         prototype: HTMLLatestCoveragesElement;
         new (): HTMLLatestCoveragesElement;
     };
+    interface HTMLReadrHeaderElement extends Components.ReadrHeader, HTMLStencilElement {
+    }
+    var HTMLReadrHeaderElement: {
+        prototype: HTMLReadrHeaderElement;
+        new (): HTMLReadrHeaderElement;
+    };
     interface HTMLElementTagNameMap {
         "latest-coverages": HTMLLatestCoveragesElement;
+        "readr-header": HTMLReadrHeaderElement;
     }
 }
 declare namespace LocalJSX {
     interface LatestCoverages {
     }
+    interface ReadrHeader {
+    }
     interface IntrinsicElements {
         "latest-coverages": LatestCoverages;
+        "readr-header": ReadrHeader;
     }
 }
 export { LocalJSX as JSX };
@@ -32,6 +44,7 @@ declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
             "latest-coverages": LocalJSX.LatestCoverages & JSXBase.HTMLAttributes<HTMLLatestCoveragesElement>;
+            "readr-header": LocalJSX.ReadrHeader & JSXBase.HTMLAttributes<HTMLReadrHeaderElement>;
         }
     }
 }
