8af0043c93e562e9c50b239088abd80f7abe128b
“liyibass” <liyibass@mirrormedia.mg>
2020-11-06 17:18:02 +0800

    build:complete mobile RelativeArticles structure.

--- Zhou-Zhi-Fei/components/RelativeArticles/Article.vue
+++ Zhou-Zhi-Fei/components/RelativeArticles/Article.vue
@@ -1,14 +1,48 @@
 <template>
-    <div class="RelativeArticles">I am RelativeArticles</div>
+    <div class="Article">
+        <div class="Article__image_container"></div>
+
+        <div class="Article__title">{{ article.title }}</div>
+
+        <div class="Article__description">{{ article.description }}</div>
+    </div>
 </template>
 
 <script>
-export default {}
+export default {
+    props: ['article'],
+}
 </script>
 
 <style lang="scss" scoped>
-.RelativeArticles {
-    height: 100vh;
-    background: $lightBlue;
+.Article {
+    margin-bottom: 40px;
+    &:last-child {
+        margin-bottom: 0px;
+    }
+
+    &__image_container {
+        width: 280px;
+        height: 187px;
+        border: solid 1px #ffffff;
+
+        margin-bottom: 20px;
+    }
+
+    &__title {
+        font-size: 16px;
+        font-weight: 500;
+        text-align: justify;
+        color: #ffffff;
+
+        margin-bottom: 10px;
+    }
+
+    &__description {
+        font-size: 16px;
+        font-weight: normal;
+        text-align: justify;
+        color: #ffffff;
+    }
 }
 </style>
