2a490204a7c1b079997440d73b0b60d38d2132d4
kjwen <kjwen310@gmail.com>
2021-06-21 13:23:33 +0800

    feat: create basic occupation block

--- covid19-vaccines-tracker/components/InputOther.vue
+++ covid19-vaccines-tracker/components/InputOther.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="input-other">
-    <div class="input-other__county">
-      <label for="county">2. 你居住的縣市？</label>
-      <div class="input-other__county-input">
+    <section class="input-other__select county">
+      <p class="label">2. 你居住的縣市？</p>
+      <div class="input-other__select-input">
         <input
           id="county"
           v-model="countyInput"
@@ -21,7 +21,64 @@
           </li>
         </ul>
       </div>
-    </div>
+    </section>
+    <section class="input-other__select occupation">
+      <p class="label">3. 你的職業？</p>
+      <div class="input-other__select-input occupation-input">
+        <input
+          id="occupation"
+          v-model="occupationInput"
+          type="text"
+          placeholder="請選擇"
+          autocomplete="off"
+          disabled
+        />
+        <ul v-if="shouldShowOccupationList">
+          <li
+            v-for="option in mockOptions"
+            :key="option"
+            @click="setOccupationInput(option)"
+          >
+            {{ option }}
+          </li>
+        </ul>
+        <div
+          class="arrow"
+          :class="{ rotate: openOccupationList }"
+          @click="toggleOccupationIcon"
+        />
+      </div>
+    </section>
+    <section class="input-other__injection">
+      <p class="label">5. 你是否已經打過第一劑？</p>
+      <div class="input-other__injection--no">
+        <input
+          v-model="injectionInput"
+          type="radio"
+          value="n"
+          class="injection-input"
+        />
+        <label>否，還沒打過</label>
+      </div>
+      <div class="input-other__injection--yes">
+        <input
+          v-model="injectionInput"
+          type="radio"
+          value="y"
+          class="injection-input"
+        />
+        <label>是，已經打過</label>
+      </div>
+      <div v-if="injectionInput === 'y'" class="input-other__injection--year">
+        <label for="yearInput">請輸入注射時間</label>
+        <input
+          id="yearInput"
+          v-model="injectionYearInput"
+          type="text"
+          placeholder="YYYY / MM / DD"
+        />
+      </div>
+    </section>
     <div class="input-other__btns">
       <button
         type="button"
@@ -36,47 +93,36 @@
 </template>
 
 <script>
+import { COUNTIES } from '~/constants'
+
 export default {
   data() {
     return {
       countyInput: '',
       currentCountyInput: undefined,
-      counties: [
-        '台北市',
-        '台中市',
-        '台南市',
-        '新北市',
-        '基隆市',
-        '桃園市',
-        '高雄市',
-        '新竹縣',
-        '苗栗縣',
-        '彰化縣',
-        '南投縣',
-        '雲林縣',
-        '嘉義縣',
-        '屏東縣',
-        '宜蘭縣',
-        '台東縣',
-        '花蓮縣',
-        '澎湖縣',
-        '金門縣',
-        '連江縣',
-        '臺北市',
-        '臺中市',
-        '臺南市',
-        '臺東縣',
+      occupationInput: undefined,
+      injectionInput: undefined,
+      injectionYearInput: undefined,
+      openOccupationList: false,
+      mockOptions: [
+        '中央及地方政府防疫人員',
+        '國際航空機組員',
+        '國際商船船員',
+        '防疫車隊駕駛',
       ],
     }
   },
   computed: {
     matchedCounty() {
-      return this.counties.filter((county) =>
+      return COUNTIES.filter((county) =>
         county.includes(this.currentCountyInput)
       )
     },
+    shouldShowOccupationList() {
+      return this.mockOptions.length && this.openOccupationList
+    },
     shouldShowNextBtn() {
-      return this.county !== ''
+      return this.countyInput && this.injectionInput
     },
   },
   methods: {
@@ -89,6 +135,13 @@ export default {
       this.currentCountyInput = undefined
       this.countyInput = county
     },
+    setOccupationInput(option) {
+      this.occupationInput = option
+      this.openOccupationList = false
+    },
+    toggleOccupationIcon() {
+      this.openOccupationList = !this.openOccupationList
+    },
   },
 }
 </script>
@@ -97,16 +150,10 @@ export default {
 .input-other {
   max-width: 320px;
   margin: 0 auto;
-  &__county {
+  &__select {
     display: flex;
     flex-direction: column;
-    margin: 0 0 48px;
-    label {
-      font-size: 18px;
-      line-height: 2;
-      color: #000928;
-      margin: 0 0 4px;
-    }
+    margin: 0 0 24px;
     &-input {
       border: 1px solid #e0e0e0;
       border-radius: 6px;
@@ -151,6 +198,58 @@ export default {
       }
     }
   }
+  &__injection {
+    margin: 0 0 48px;
+    @include media-breakpoint-up(md) {
+      margin: 0 0 54px;
+    }
+    &--no,
+    &--yes {
+      display: flex;
+      align-items: center;
+      .injection-input {
+        width: 18px;
+        height: 18px;
+      }
+      label {
+        font-size: 18px;
+        line-height: 18px;
+        color: #000928;
+        margin: 0 0 0 8px;
+      }
+    }
+    &--no {
+      margin: 0 0 18px;
+      @include media-breakpoint-up(md) {
+        margin: 0 0 22px;
+      }
+    }
+    &--year {
+      display: flex;
+      flex-direction: column;
+      margin: 12px 0 0;
+      label {
+        font-size: 18px;
+        line-height: 2;
+        color: #000928;
+        margin: 0 0 4px;
+        @include media-breakpoint-up(md) {
+          margin: 0 0 8px;
+        }
+      }
+      input[type='text'] {
+        font-size: 18px;
+        line-height: 1.5;
+        border: 1px solid #e0e0e0;
+        border-radius: 6px;
+        padding: 12px 16px;
+        &:focus {
+          outline: none;
+          border: 1px solid #04295e;
+        }
+      }
+    }
+  }
   &__btns {
     width: 100%;
     text-align: center;
@@ -159,6 +258,33 @@ export default {
     }
     &--secondary {
       margin: 0 auto 24px;
+    }
+  }
+  .label {
+    font-size: 18px;
+    line-height: 2;
+    color: #000928;
+    margin: 0 0 4px;
+    @include media-breakpoint-up(md) {
+      margin: 0 0 8px;
+    }
+  }
+  .occupation-input {
+    position: relative;
+    .arrow {
+      position: absolute;
+      top: 22px;
+      right: 20px;
+      width: 0;
+      height: 0;
+      border-style: solid;
+      border-width: 10px 10px 0 10px;
+      border-color: #04295e transparent transparent transparent;
+      cursor: pointer;
+      transition: all 0.3s ease-in-out;
+    }
+    .rotate {
+      transform: rotate(180deg);
     }
   }
 }
