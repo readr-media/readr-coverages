66c3b9a89c265268462d0bde240183cf15bbab18
kjwen <kjwen310@gmail.com>
2021-06-22 17:07:36 +0800

    feat: add you-might-want-to-know block

--- covid19-vaccines-tracker/components/Result.vue
+++ covid19-vaccines-tracker/components/Result.vue
@@ -9,17 +9,60 @@
         </button>
       </div>
     </div>
+    <div class="result__faq">
+      <h2>你可能還想知道</h2>
+      <ul>
+        <li
+          v-for="item in mockList"
+          :key="item.title"
+          class="result__faq__toggle-card"
+        >
+          <UiToggleCard
+            :title="item.title"
+            :content="item.content"
+            :graphUrl="item.url"
+          />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import ResultBoard from '~/components/ResultBoard.vue'
 import EmailBoard from '~/components/EmailBoard.vue'
+import UiToggleCard from '~/components/UiToggleCard.vue'
 
 export default {
   components: {
     ResultBoard,
     EmailBoard,
+    UiToggleCard,
+  },
+  data() {
+    return {
+      mockList: [
+        {
+          title:
+            '勞工前往接種COVID-19疫苗，以及接種後擔心有不適反應，可否請疫苗接種假？',
+          content:
+            '指揮中心建議，接種第 2 劑AZ疫苗的時間最好是間隔 8 至 12 周，如果是 3 月 22 日施打第一劑，12 周後的時間點會落在 6 月 13 日。不過台灣感染症醫學會名譽理事長黃立民指出，國外數據顯示，間隔12到16周打第二劑，其實和間隔10到12周的效果差不多，但因為國外幾乎沒有，建議最晚就是間隔 16 周。因此，在 7 月 11 日前打第二劑都還行。',
+          url: '',
+        },
+        {
+          title: '接種疫苗需要帶什麼證件？',
+          content:
+            '指揮中心建議，接種第 2 劑AZ疫苗的時間最好是間隔 8 至 12 周，如果是 3 月 22 日施打第一劑，12 周後的時間點會落在 6 月 13 日。不過台灣感染症醫學會名譽理事長黃立民指出，國外數據顯示，間隔12到16周打第二劑，其實和間隔10到12周的效果差不多，但因為國外幾乎沒有',
+          url: 'xxxxxxx',
+        },
+        {
+          title: '勞工前往接種COVID-19疫苗，可否請疫苗接種假？',
+          content:
+            '不過台灣感染症醫學會名譽理事長黃立民指出，國外數據顯示，間隔12到16周打第二劑，其實和間隔10到12周的效果差不多，但因為國外幾乎沒有，建議最晚就是間隔 16 周。因此，在 7 月 11 日前打第二劑都還行。',
+          url: '',
+        },
+      ],
+    }
   },
   methods: {
     handleSeachAgain() {
@@ -44,16 +87,36 @@ export default {
     min-height: calc(100vh - 152px - 120px);
   }
   &__info {
+    background-color: #fff;
     border: 2px solid #000928;
     border-radius: 6px;
     max-width: 640px;
-    margin: 0 auto;
+    margin: 0 auto 56px;
+    @include media-breakpoint-up(md) {
+      margin: 0 auto 120px;
+    }
     &-btn {
       margin: 24px 0 20px;
       text-align: center;
       @include media-breakpoint-up(md) {
         margin: 24px 0 32px;
       }
+    }
+  }
+  &__faq {
+    max-width: 600px;
+    margin: 0 auto 56px;
+    @include media-breakpoint-up(md) {
+      margin: 0 auto 120px;
+    }
+    h2 {
+      margin: 0 0 12px;
+      @include media-breakpoint-up(md) {
+        margin: 0 0 24px;
+      }
+    }
+    &__toggle-card {
+      margin: 0 0 16px;
     }
   }
 }
---
+++ covid19-vaccines-tracker/components/UiToggleCard.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="toggle-card">
+    <div class="toggle-card__title" @click="toggleContent">
+      <p>{{ title }}</p>
+      <div class="toggle-card__title-img">
+        <img
+          v-if="shouldShowContent"
+          src="~static/images/open-icon.svg"
+          alt="toggle icon"
+        />
+        <img v-else src="~static/images/close-icon.svg" alt="toggle icon" />
+      </div>
+    </div>
+    <div v-if="shouldShowContent" class="toggle-card__content">
+      <div v-if="graphUrl" class="toggle-card__content-graph"></div>
+      <p class="toggle-card__content-text">{{ content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    content: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    graphUrl: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      shouldShowContent: false,
+    }
+  },
+  methods: {
+    toggleContent() {
+      this.shouldShowContent = !this.shouldShowContent
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.toggle-card {
+  background-color: #fff;
+  border-radius: 6px;
+  padding: 12px 16px;
+  box-shadow: 0 4px 4px -2px rgba(0, 0, 0, 0.1);
+  @include media-breakpoint-up(md) {
+    padding: 20px 24px;
+  }
+  &__title {
+    display: flex;
+    justify-content: space-between;
+    align-items: start;
+    cursor: pointer;
+    p {
+      max-width: 90%;
+      font-size: 16px;
+      line-height: 1.5;
+      color: #000928;
+      opacity: 0.87;
+    }
+    &-img {
+      width: 22px;
+      height: 24px;
+      display: flex;
+      align-items: center;
+      img {
+        width: 100%;
+      }
+    }
+  }
+  &__content {
+    margin: 12px 0 0;
+    padding: 12px 0 0;
+    border-top: 1px solid #e0e0e0;
+    &-graph {
+      width: 100%;
+      height: 400px;
+      background-color: #c4c4c4;
+      margin: 0 0 12px;
+    }
+    &-text {
+      font-size: 16px;
+      line-height: 2;
+      color: #000928;
+      opacity: 0.66;
+    }
+  }
+}
+</style>
