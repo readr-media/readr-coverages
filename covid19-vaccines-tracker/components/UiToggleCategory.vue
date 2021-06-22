fd1e37e94a73eaf8a3747047d404d121209b85b4
kjwen <kjwen310@gmail.com>
2021-06-22 17:58:54 +0800

    feat: add basic more-faq block

---
+++ covid19-vaccines-tracker/components/UiToggleCategory.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <UiToggleCard :title="''" :content="''" :graphUrl="''" />
+  </div>
+</template>
+
+<script>
+import UiToggleCard from '~/components/UiToggleCard.vue'
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    items: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
+  components: {
+    UiToggleCard,
+  },
+}
+</script>
