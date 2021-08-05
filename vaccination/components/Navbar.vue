5f208ca278827ce952dc2c42a23364a768cf518e
kjwen <kjwen310@gmail.com>
2021-08-05 13:28:37 +0800

    build(vaccination): init project

---
+++ vaccination/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="Navbar">
+    <readr-header
+      @readrLogoLinkClick="gaClickHandler('back to home')"
+      @shareFacebookLinkClick="gaClickHandler('share to fb')"
+      @shareLineLinkClick="gaClickHandler('share to line')"
+    />
+    <div class="Navbar__anchor"></div>
+  </div>
+</template>
+
+<script>
+import gaMixin from '~/mixins/gaMixin'
+
+export default {
+  mixins: [gaMixin],
+}
+</script>
+
+<style lang="scss" scoped>
+.Navbar {
+  position: relative;
+  z-index: 1000;
+  background: transparent;
+  width: 100%;
+}
+</style>
