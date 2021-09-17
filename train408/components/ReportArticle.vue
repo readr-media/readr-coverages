89372a215b6df1aac5a13da7ccfc1ca09d75fd8e
kjwen <kjwen310@gmail.com>
2021-09-17 18:39:30 +0800

    feat: add flourish graph to report area

--- train408/components/ReportArticle.vue
+++ train408/components/ReportArticle.vue
@@ -4,8 +4,10 @@
     <div>
       <h2>5 秒鐘：太魯閣號出軌事件完整還原</h2>
       <p>
-        事故發生不到1個月，花蓮地檢署偵查顯示，包商李義祥借牌投標台鐵工程，因施工進度落後，4
-        月2日違規趕工，導致他過失致死等行為起訴包商李義祥等7人以及東新營造公司，<UiAnnotation
+        事故發生不到 1
+        個月，花蓮地檢署偵查顯示，包商李義祥借牌投標台鐵工程，因施工進度落後，4
+        月 2 日違規趕工，導致他過失致死等行為起訴包商李義祥等 7
+        人以及東新營造公司，<UiAnnotation
           :text="'字裡行間'"
           :pureAnnotationText="'註解如我，以下是假字。很息不，息大手年自完年的清代已到火此子做看，世口他風停有。'"
         />
@@ -13,25 +15,40 @@
       </p>
       <div id="article-middle" class="article-middle-anchor" />
       <p>
-        兩個月後，花檢再以過失致死、偽造文書罪追加起訴1名台鐵花蓮工務段人員、以及負責工程專案管理的中棪公司專案經理。但台鐵的責任只有這樣嗎？旅客與家屬依舊渴望更多的事故真相。家屬第一時間詢問台鐵內部的調查結果，得到的回應是：「因為疫情，還沒有辦法處理，」罹難者家屬潘麗如描述。
+        兩個月後，花檢再以過失致死、偽造文書罪追加起訴 1
+        名台鐵花蓮工務段人員、以及負責工程專案管理的中棪公司專案經理。但台鐵的責任只有這樣嗎？旅客與家屬依舊渴望更多的事故真相。家屬第一時間詢問台鐵內部的調查結果，得到的回應是：「因為疫情，還沒有辦法處理，」罹難者家屬潘麗如描述。
       </p>
-      <UiReportTable class="article__table" />
+      <UiReportTable class="article__graph" />
       <p>
-        兩個月後，花檢再以過失致死、偽造文書罪追加起訴1名台鐵花蓮工務段人員、以及負責工程專案管理的中棪公司專案經理。但台鐵的責任只有這樣嗎？旅客與家屬依舊渴望更多的事故真相。家屬第一時間詢問台鐵內部的調查結果，得到的回應是：「因為疫情，還沒有辦法處理，」罹難者家屬潘麗如描述。
+        兩個月後，花檢再以過失致死、偽造文書罪追加起訴 1
+        名台鐵花蓮工務段人員、以及負責工程專案管理的中棪公司專案經理。但台鐵的責任只有這樣嗎？旅客與家屬依舊渴望更多的事故真相。家屬第一時間詢問台鐵內部的調查結果，得到的回應是：「因為疫情，還沒有辦法處理，」罹難者家屬潘麗如描述。
       </p>
       <h3>縱觀所有論壇資料</h3>
       <p>
-        事實上，交通部早在5月21日就完成了內部的行政調查報告。但直到2個月後，交通部才在7月18日公布該份報告。根據這份行政調查，從施工計劃、工地人員安全教育到邊坡防護措施，台鐵花蓮工務段、專案管理單位和監造單位「顯有疏漏」，12名台鐵相關人員僅被申誡或記過。
+        事實上，交通部早在 5 月 21 日就完成了內部的行政調查報告。但直到 2
+        個月後，交通部才在 7 月 18
+        日公布該份報告。根據這份行政調查，從施工計劃、工地人員安全教育到邊坡防護措施，台鐵花蓮工務段、專案管理單位和監造單位「顯有疏漏」，12
+        名台鐵相關人員僅被申誡或記過。
       </p>
-      <UiReportHideTable class="article__table" />
+      <UiReportHideTable class="article__graph" />
       <p>
-        事實上，交通部早在5月21日就完成了內部的行政調查報告。但直到2個月後，交通部才在7月18日公布該份報告。根據這份行政調查，從施工計劃、工地人員安全教育到邊坡防護措施，台鐵花蓮工務段、專案管理單位和監造單位「顯有疏漏」，12名台鐵相關人員僅被申誡或記過。
+        事實上，交通部早在 5 月 21 日就完成了內部的行政調查報告。但直到 2
+        個月後，交通部才在 7 月 18
+        日公布該份報告。根據這份行政調查，從施工計劃、工地人員安全教育到邊坡防護措施，台鐵花蓮工務段、專案管理單位和監造單位「顯有疏漏」，12
+        名台鐵相關人員僅被申誡或記過。
       </p>
       <div class="article__image">
         <img src="~/static/images/og.jpg" alt="image one" />
       </div>
       <p>
-        8月23日，獨立調查單位國家運輸安全委員會發布「0402台鐵第408次車清水隧道重大鐵道事故第一階段事實報告」，總算揭露更多關鍵訊息：一連串人為失誤，台鐵難辭其咎。
+        8 月 23 日，獨立調查單位國家運輸安全委員會發布「0402 台鐵第 408
+        次車清水隧道重大鐵道事故第一階段事實報告」，總算揭露更多關鍵訊息：一連串人為失誤，台鐵難辭其咎。
+      </p>
+      <!-- eslint-disable vue/no-v-html -->
+      <div v-html="iframeSrc" class="article__graph"></div>
+      <p>
+        8 月 23 日，獨立調查單位國家運輸安全委員會發布「0402 台鐵第 408
+        次車清水隧道重大鐵道事故第一階段事實報告」，總算揭露更多關鍵訊息：一連串人為失誤，台鐵難辭其咎。
       </p>
     </div>
   </section>
@@ -49,6 +66,26 @@ export default {
     UiAnnotation,
     UiReportTable,
     UiReportHideTable,
+  },
+  data() {
+    return {
+      iframeSrc: `
+        <iframe
+          src='https://flo.uri.sh/visualisation/7285118/embed'
+          title='Interactive or visual content'
+          class='flourish-embed-iframe'
+          frameborder='0'
+          scrolling='no'
+          style='width:100%;height:600px;'
+          sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'
+        ></iframe>
+        <div style='width:100%!;margin-top:4px!important;text-align:right!important;'>
+          <a class='flourish-credit' href='https://public.flourish.studio/visualisation/7285118/?utm_source=embed&utm_campaign=visualisation/7285118' target='_top' style='text-decoration:none!important'>
+            <img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'>
+          </a>
+        </div>
+      `,
+    }
   },
   mounted() {
     const scrollerCredit = scrollama()
@@ -134,7 +171,7 @@ export default {
       object-position: center;
     }
   }
-  &__table {
+  &__graph {
     width: calc(100% + 40px);
     transform: translateX(-20px);
     margin: 32px 0 16px;
